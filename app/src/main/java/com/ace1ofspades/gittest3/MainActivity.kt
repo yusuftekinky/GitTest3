d2c3e8694e9183cb27d040498bdb5607260436bd
Yusuf Tekin <yusuf.tekin@kofteciyusuf.com.tr>
2021-10-18 20:56:08 +0300

    inital2

--- app/src/main/java/com/ace1ofspades/gittest3/MainActivity.kt
+++ app/src/main/java/com/ace1ofspades/gittest3/MainActivity.kt
@@ -2,10 +2,12 @@ package com.ace1ofspades.gittest3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ace1ofspades.testlib.Alert
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Alert.showMessage(this)
     }
 }
